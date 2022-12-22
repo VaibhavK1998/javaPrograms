80891f16c28c1e63d1026cc27492b1f91ab93d21
Vaibhav <kitukale.vaibhav26@outlook.com>
2022-12-22 23:19:44 +0530

    c1

--- src/LoopsInJava/AssigmentExa.java
+++ src/LoopsInJava/AssigmentExa.java
@@ -3,19 +3,19 @@ package LoopsInJava;
 public class AssigmentExa {
 
 	public static void main(String[] args) {
-		for(int a=1; a<=2;a++) {
-for(int b=1;b==a;b++) {
-for(int c=1;c<=b;c++)	{
-System.out.print(c);
-        
-           }
-System.out.print(b);
-          }
-	
-	System.out.print(a);
-	
-		}	
-	
-	}	
-	
+		for (int a = 1; a < 4; a++) {
+			for (int b = 1; b <= a; b++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+
+		for (int c = 1; c < 5; c++) {
+ for(int d=4; d>=c;d--){
+	 System.out.print("*");
+ }System.out.println();
+		}
+
+	}
 }
